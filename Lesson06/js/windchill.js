7b45055b70443eed1a478e5f57ed4b2c02b4148f
pat19001 <pat19001@byui.edu>
2020-10-24 20:01:12 -0600

    windchill

--- Lesson06/js/windchill.js
+++ Lesson06/js/windchill.js
@@ -1,7 +1,13 @@
 /* f=35.74+0.6215t−35.75s0.16+0.4275ts0.16 */
 window.addEventListener('load', (event)=>{
-  var temp = document.querySelector("#ht")
-  var wspeed = document.querySelector("ws")
-  var wchill = ((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wspeed, 0.16)))
-  console.log(wchill)
+  var temp = document.querySelector('#ht').innerHTML;
+  var wspeed = document.querySelector('#ws').innerHTML;
+  var wchill = Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(wspeed, 0.16) + 0.4275 * temp * Math.pow(wspeed, 0.16));
+  const wc = document.querySelector('#windchill');
+  if (temp < 50 && wspeed > 3.0){
+    wc.textContent = wchill;
+  }
+  else {
+    wc.textContent = "N/A"
+  }
 })
