859ed8f883d840467b514d8a9243ab190cd5b443
pat19001 <pat19001@byui.edu>
2020-11-23 10:02:53 -0700

    Co-authored-by: Moita, Joao <jmoita3@byui.edu>

--- practice/js/iceperson.js
+++ practice/js/iceperson.js
@@ -1,4 +1,6 @@
-const requestURL = '/Users/hollypatterson77/Downloads/Local_Repository/pat19001.github.io/practice/json/person.json';
+// const requestURL = '/practice/json/person.json';
+
+const requestURL = 'https://www.ahfx.com/person.php';
 
 fetch(requestURL)
   .then(function (response) {
@@ -8,40 +10,50 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     
     const person = jsonObject['person']
-    const marriage  = jsonObject['marriage'];
-    const personal  = jsonObject['personal'];
-    const online  = jsonObject['online_info'];
+    const marriage  = person['marriage'];
+    const personal  = person['personal'];
+    const online  = person['online_info'];
 
-    var list = [marriage, personal, online];
-
-    for (i = 0; i < list.length; i++){
       let card = document.createElement('section');
-      let name = document.createElement('h2');
+      let fname = document.createElement('h2');
+      let lname = document.createElement('h2');
       let pass = document.createElement('p');
       let eye = document.createElement('p');
       let city = document.createElement('p');
+      let country = document.createElement('p');
       let children = document.createElement('p');
       let ip = document.createElement('p');
       let image = document.createElement('img');
 
 
-      // h2.textContent = prophets[i].name + ' ' + person[i].;
+      fname.textContent = personal.name;
+      lname.textContent = personal.last_name;
+      eye.textContent = personal.eye_color;
+      city.textContent = personal.city;
+      country.textContent = personal.country;
+
+      children.textContent = marriage.children;
+
+      ip.textContent = online.ip_address;
+
+      image.setAttribute('src', 'https://thispersondoesnotexist.com/image');
       // p1.textContent = "Date of Birth: " + prophets[i].birthdate
       // p2.textContent = "Place of Birth: " + prophets[i].birthplace
 
 
-      card.appendChild(name);
+      card.appendChild(fname);
+      card.appendChild(lname);
       card.appendChild(pass);
       card.appendChild(eye);
       card.appendChild(city);
       card.appendChild(children);
-      card.appendChild(city);
-      card.appendChild(city);
+      card.appendChild(ip);
+      card.appendChild(country);
+      card.appendChild(image);
+
 
       document.querySelector('div.cards').appendChild(card);
       // image.setAttribute('src', prophets[i].imageurl);
       // image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + i)
 
-    }
-  console.log(person);
-  })
+    })
