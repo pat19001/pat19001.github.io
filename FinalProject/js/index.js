cfcd2480fa9752836f3c84155fdabfef5ec5f3d4
pat19001 <pat19001@byui.edu>
2020-12-13 23:41:03 -0700

    json typo fix

--- FinalProject/js/index.js
+++ FinalProject/js/index.js
@@ -55,23 +55,64 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     
     const info = jsonObject['temple']
+      let sections = document.createElement('div')
 
       let provo = document.createElement('div');
-      let tucson = document.createElement('div');
-      let houston = document.createElement('div');
-      let baton = document.createElement('div');
+      let pname = document.createElement('h2');
+      let paddress = document.createElement('p');
+      let pstreet = document.createElement('p');
+      let pcity = document.createElement('p');
 
+      
+      pname.textContent = "Temple: " + info.provo.name;
+      provo.appendChild(pname);
+      paddress.textContent = "Address";
+      provo.appendChild(paddress);
+      paddress.textContent = info.provo.address;
+      provo.appendChild(pstreet);
+      pcity.textContent = info.provo.city + ', ' + info.provo.state + " " + info.provo.zipcode;
+      provo.appendChild(pcity);
+
+      let tucson = document.createElement('div');
+      let tname = document.createElement('h2');
+      let taddress = document.createElement('p');
+      let tcity = document.createElement('p');
+
+      tname.textContent = "Temple: " + info.tucson.name;
+      tucson.appendChild(tname);
+      taddress.textContent = "Address";
+      tucson.appendChild(taddress);
+      taddress.textContent = info.tucson.address;
+      tucson.appendChild(tstreet);
+      tcity.textContent = info.tucson.city + ', ' + info.tucson.state + " " + info.tucson.zipcode;
+      tucson.appendChild(tcity);
+
+      let houston = document.createElement('div');
+      let hname = document.createElement('h2');
+      let haddress = document.createElement('p');
+      let hcity = document.createElement('p');
+      let hstate = document.createElement('p');
+      let hzipcode = document.createElement('p');
+
+      let baton = document.createElement('div');
+      let bname = document.createElement('h2');
+      let baddress = document.createElement('p');
+      let bcity = document.createElement('p');
+      let bstate = document.createElement('p');
+      let bzipcode = document.createElement('p');
+
+      sections.setAttribute('id','sections')
       provo.setAttribute('class', 'location');
       tucson.setAttribute('class', 'location');
       houston.setAttribute('class', 'location');
       baton.setAttribute('class', 'location');
 
-      provo.textContent = "Temple: " + provo.name;
+      
 
-      document.querySelector('div.sections').appendChild(provo);
-      document.querySelector('div.sections').appendChild(tucson);
-      document.querySelector('div.sections').appendChild(houston);
-      document.querySelector('div.sections').appendChild(baton);
+      document.querySelector('main').appendChild(sections);
 
-
+      sections.appendChild(provo);
+      sections.appendChild(tucson);     
+      sections.appendChild(houston);   
+      sections.appendChild(baton);
     })
