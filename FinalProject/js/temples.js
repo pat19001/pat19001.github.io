85316fca8afd16d159b3446eaffc406144b2af5b
pat19001 <pat19001@byui.edu>
2020-12-14 23:58:33 -0700

    weathersummary

--- FinalProject/js/temples.js
+++ FinalProject/js/temples.js
@@ -219,4 +219,44 @@ fetch(requestURL)
       sections.appendChild(tucson);     
       sections.appendChild(houston);   
       sections.appendChild(baton);
-    })
+    });
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=84604,us&appid=c1820e7aeb32dab60966a969b49d248d&units=imperial";
+    let weatherRequest = new XMLHttpRequest();
+    weatherRequest.open('Get', apiURL, true);
+    weatherRequest.send();
+    fetch(apiURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        // console.table(jsonObject);
+        let weatherData = JSON.parse(weatherRequest.responseText);
+        //console.log(weatherData);
+    
+        // const w = jsonObject['weather'];
+        // const m = jsonObject['main'];
+        // const windt = jsonObject['wind'];
+    
+        let curr = document.getElementById('curr');
+        let high = document.getElementById('ht');
+        // let windc = document.getElementById('windchill');
+        let h = document.getElementById('humid');
+        let ws = document.getElementById('ws');
+        let wc = document.getElementById('windchill');
+    
+        curr.textContent = "Currently: " + weatherData.weather[0].main;
+        high.textContent = Math.round(weatherData.main.temp);
+        h.textContent = "Humidity: " + weatherData.main.humidity + "%";
+        ws.textContent = Math.round(weatherData.wind.speed);
+    
+        let temp = high.textContent;
+        let wspeed = ws.textContent;
+        //console.log(temp,wspeed);
+        let wchill = Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(wspeed, 0.16) + 0.4275 * temp * Math.pow(wspeed, 0.16));
+        if (temp < 50 && wspeed > 3.0) {
+          wc.textContent = wchill;
+        } else {
+          wc.textContent = "N/A"
+        }
+        //console.log(wchill);
+      });
